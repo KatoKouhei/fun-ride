c3a5188f273cafd19d1d28003889982243524296
KatoKouhei <siroyama2580@yahoo.co.jp>
2019-12-11 22:29:09 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
