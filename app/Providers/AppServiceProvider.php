674cc10074ffa8a55101f78d7ae0be46058d3361
KatoKouhei <siroyama2580@yahoo.co.jp>
2019-12-11 22:39:58 +0900

    https2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
-        if (request()->isSecure()) {
+        if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
     }
